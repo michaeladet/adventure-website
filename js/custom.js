f7d62b5ea62175d9bc090402fac782836fc4a047
michaeladet <michaeladet184@gmail.com>
2022-11-27 15:26:15 +0100

    First commit

---
+++ js/custom.js
@@ -0,0 +1,32 @@
+
+
+
+
+$(document).ready(function() {
+
+ 
+
+
+ 
+
+
+ 
+
+
+   /* Owl Carousel
+    -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-work").owlCarousel({
+      autoPlay: 3000,
+      items : 4,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3],
+    });
+  });
+  
+
+
+ 
+
+  });
+
